45f77c4858ca657049ed52f1ed957d303a92909b
임한솔 <hansol@imhansol-ui-MacBookPro.local>
2022-11-24 01:26:00 +0900

    deck css finished

--- final-pjt-front/src/components/MyDeck.vue
+++ final-pjt-front/src/components/MyDeck.vue
@@ -4,6 +4,15 @@
       My Deck
     </h3>
     <b-list-group-item>
+      <b-list-group-item>
+        <b-row class="text-start mx-1 rounded">
+          <b-col cols="2">card</b-col>
+          <b-col cols="5">name</b-col>
+          <b-col cols="2">type</b-col>
+          <b-col cols="1">AD</b-col>
+          <b-col cols="1">HP</b-col>
+        </b-row>
+      </b-list-group-item>
       <MyCard
        v-for="(card, index) in userCards"
        :key="`${card.id}-${index}`"
