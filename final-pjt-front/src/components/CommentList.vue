fcf35b83b746d9d8b5061e7ef5414c22a3746efe
PPPARK <sdc00039@gamil.com>
2022-11-23 16:54:14 +0900

    'css_4'

--- final-pjt-front/src/components/CommentList.vue
+++ final-pjt-front/src/components/CommentList.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <h3>
-      댓글 폼
-    </h3>
-    <CommentItem v-for="(comment, index) in comments" :key="`${comment}${index}`" :comment="comment" />
+    <b-list-group v-if="comments.length">
+      <b-list-group-item>
+        <b-row class="text-center">
+          <b-col cols="2" class="ms-2">작성자</b-col>
+          <b-col cols="7" style="text-start">내용</b-col>
+          <b-col cols="2">생성일자</b-col>
+        </b-row>
+        <hr>
+        <CommentItem v-for="(comment, index) in comments" :key="`${comment}${index}`" :comment="comment" />
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
